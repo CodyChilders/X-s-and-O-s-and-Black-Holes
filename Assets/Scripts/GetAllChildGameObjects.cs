e32acab25b9f9885f1fe73d2b664b50f159246db
CodyIsAwesome <cchilder@ucsc.edu>
2015-09-22 15:06:44 -0700

    Stole a utility script from Eruption
    This way I can get child game objects faster.  I'll need that feature
    later

---
+++ Assets/Scripts/GetAllChildGameObjects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetAllChildGameObjects : MonoBehaviour 
+{
+    public static GameObject[] GetAllChildren(GameObject parent)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>();
+        GameObject[] objects = new GameObject[children.Length];
+        for (int i = 0; i < children.Length; i++)
+        {
+            Transform child = children[i];
+            objects[i] = child.gameObject;
+        }
+        return objects;
+    }
+}
