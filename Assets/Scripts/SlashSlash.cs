e6b079eed07a982afc43da84c25cf677cd3dea4f
CodyIsAwesome <cchilder@ucsc.edu>
2015-09-23 20:01:36 -0700

    Bug fixes
    I have to figure out how to get the gitignore working right again
    though, most of this shouldn't need to go in.

---
+++ Assets/Scripts/SlashSlash.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlashSlash : MonoBehaviour
+{
+    [TextArea(3, 10)]
+    public string comment = "";
+
+    /****************************************************
+     * This variable affects the comment field when the *
+     * game is played.  If you want to save a bit of    *
+     * memory, this deletes it right away so it gets    *
+     * garbage collected in the beginning of scene load *
+     ***************************************************/
+    public bool deleteStringOnPlay = false;
+
+    /****************************************************
+     * This variable controls an extra parameter on     *
+     * deleteStringOnPlay.  This stops that action from *
+     * happening in debug mode, allowing you to see the *
+     * comment if you pause the game and view while     *
+     * running in the editor.                           *
+     ***************************************************/
+    public bool deleteOnlyInReleaseMode = false;
+
+    void Awake()
+    {
+        //do not delete this comment
+        if (!deleteStringOnPlay)
+        {
+            return;
+        }
+        bool deleteFlag = true; //this flag can be set to false under certain conditions
+        if (deleteOnlyInReleaseMode)
+        {
+            if (!Debug.isDebugBuild)
+                deleteFlag = false;
+        }
+        //free up the memory if all checks pass
+        if (deleteFlag)
+        {
+            comment = "";
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/SlashSlashEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SlashSlash))]
+public class SlashSlashEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SlashSlash myTarget = (SlashSlash)target;
+
+        bool deleteOnPlay = EditorGUILayout.Toggle("Free memory on play", myTarget.deleteStringOnPlay);
+        myTarget.deleteStringOnPlay = deleteOnPlay;
+        if (deleteOnPlay)
+        {
+            myTarget.deleteOnlyInReleaseMode = EditorGUILayout.Toggle("Free memory only in release mode", myTarget.deleteOnlyInReleaseMode);
+        }
+        //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+    }
+}
