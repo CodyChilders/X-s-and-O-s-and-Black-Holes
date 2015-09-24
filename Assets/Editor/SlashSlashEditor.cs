e3fd5a55bc31ff5feea297268eca4a96b3ab97f7
CodyIsAwesome <cchilder@ucsc.edu>
2015-09-23 20:02:27 -0700

    Added utility scripts to comment on objects
    There is a new component "SlashSlash" and matching editor script to make
    comments on objects possible, with options to delete it on startup to
    save a bit of memory.

---
+++ Assets/Editor/SlashSlashEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SlashSlash)), CanEditMultipleObjects]
+public class SlashSlashEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SlashSlash myTarget = (SlashSlash)target;
+
+        EditorGUILayout.LabelField("Comment");
+        EditorStyles.textField.wordWrap = true;
+        myTarget.comment = EditorGUILayout.TextField("", myTarget.comment, GUILayout.MaxHeight(100));
+
+        bool deleteOnPlay = EditorGUILayout.Toggle("Free memory on play", myTarget.deleteStringOnPlay);
+        myTarget.deleteStringOnPlay = deleteOnPlay;
+        if (deleteOnPlay)
+        {
+            myTarget.deleteOnlyInReleaseMode = EditorGUILayout.Toggle("Free memory only in release mode", myTarget.deleteOnlyInReleaseMode);
+        }
+        //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+    }
+}
