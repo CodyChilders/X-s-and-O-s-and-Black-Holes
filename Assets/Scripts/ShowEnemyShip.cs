d380c530053575a3c420e1b52ba0e14435502e42
CodyIsAwesome <cchilder@ucsc.edu>
2015-09-27 19:41:25 -0700

    Ship marker doesn't render on enemies screen
    It does, however, render if they are directly behind them.  Still don't
    know how to fix that.

--- Assets/Scripts/ShowEnemyShip.cs
+++ Assets/Scripts/ShowEnemyShip.cs
@@ -18,6 +18,8 @@ public class ShowEnemyShip : MonoBehaviour
 
     public Font font;
 
+    public int playerNumber = 0;
+
     private GUIStyle gs = new GUIStyle();
     private GUIStyle textStyle = new GUIStyle();
 
@@ -34,6 +36,11 @@ public class ShowEnemyShip : MonoBehaviour
         Vector2 displayPoint = new Vector2(screenPoint.x, Screen.height - screenPoint.y);
         displayPoint.x -= imageWidth  / 2f;
         displayPoint.y -= imageHeight / 2f;
+        //it might be on the screen, but on the other player's side.  skip the rest of the method if so
+        if (ShouldSkipDrawingHud(displayPoint))
+        {
+            return;
+        }
         GUI.color = targetColor;
         GUI.Box(new Rect(displayPoint.x, displayPoint.y, imageWidth, imageHeight), 
                 bullseye,
@@ -45,6 +52,26 @@ public class ShowEnemyShip : MonoBehaviour
         GUI.Label(new Rect(displayPoint.x, displayPoint.y + imageHeight, 1000, 1000),
                   sb.ToString(),
                   textStyle);
+    }
+
+    private bool ShouldSkipDrawingHud(Vector2 displayPoint)
+    {
+        float xCoordinate = displayPoint.x + imageWidth / 2;
+        if (playerNumber == 1)
+        {
+            if (xCoordinate > Screen.width / 2)
+                return true;
+        }
+        else if(playerNumber == 2)
+        {
+            if (xCoordinate < Screen.width / 2)
+                return true;
+        }
+        else
+        {
+            return true;
+        }
+        return false;
     }
 
     private string GetDistance()
